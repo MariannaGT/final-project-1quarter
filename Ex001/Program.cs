a089430ae802aa052e11aac3919deaf4d7717127
Mary <marianna18031988@mail.ru>
2022-11-12 12:09:09 +0300

    Добавили решение

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("12Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. 
+// Примеры:
+// ["hello", "2", "world", -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array1 = new string[5] {"Russia", "525", "hello", "Kazan", "PB"};
+string[] array2 = new string[array1.Length];
+//void SecondArrayWithIF(string[] array1, string[] array2)
+void  FillArrayNumbers(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArrayNumbers(array1, array2);
+PrintArray(array2);
