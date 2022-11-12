bb843246d65eaf3ea3f6a5078911247d383bb531
Mary <marianna18031988@mail.ru>
2022-11-12 12:11:48 +0300

    Добавили массив

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -6,9 +6,8 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
-string[] array1 = new string[5] {"Russia", "525", "hello", "Kazan", "PB"};
+string[] array1 = new string[5] {"TOMI", "ANN", "MARYA", "JOJIT", "PB"};
 string[] array2 = new string[array1.Length];
-//void SecondArrayWithIF(string[] array1, string[] array2)
 void  FillArrayNumbers(string[] array1, string[] array2)
 {
     int count = 0;
